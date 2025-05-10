d03d00bd14726fcb6605f3fa188c1c86b3a2aa8f
MVBPinho <marcos.vb.pinho@live.com>
2025-05-10 15:43:47 -0300

    Criação do projeto ms-login e criação do contrato do swagger.yaml

---
+++ ms-login/src/main/java/com/fiap/ms/login/adapters/out/repository/entity/LoginEntity.java
@@ -0,0 +1,24 @@
+package com.fiap.ms.login.adapters.out.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_login")
+public class LoginEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String login;
+
+    private String senha;
+}
